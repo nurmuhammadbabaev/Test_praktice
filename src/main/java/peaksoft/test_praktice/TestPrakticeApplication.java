d2345e6cd0059cd6ab81187ff8ff132b9e604c4a
Nurmuhammed <nurmuhammadbabaev@gmail.com>
2022-04-01 15:09:35 +0600

    sfxdcgb

--- src/main/java/peaksoft/test_praktice/TestPrakticeApplication.java
+++ src/main/java/peaksoft/test_praktice/TestPrakticeApplication.java
@@ -9,6 +9,7 @@ public class TestPrakticeApplication {
     public static void main(String[] args) {
         System.out.println("sdgfch vb");
         System.out.println("gbedtgr vre");
+        System.out.println("gbedtgr vre");
         SpringApplication.run(TestPrakticeApplication.class, args);
     }
 
