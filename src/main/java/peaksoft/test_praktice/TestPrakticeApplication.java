7ec8d9132d38ac45fe27641881574c3c21a49872
Nurmuhammed <nurmuhammadbabaev@gmail.com>
2022-04-01 12:27:10 +0600

    2fedsv

---
+++ src/main/java/peaksoft/test_praktice/TestPrakticeApplication.java
@@ -0,0 +1,13 @@
+package peaksoft.test_praktice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPrakticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPrakticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/peaksoft/test_praktice/TestPrakticeApplicationTests.java
@@ -0,0 +1,13 @@
+package peaksoft.test_praktice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPrakticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
